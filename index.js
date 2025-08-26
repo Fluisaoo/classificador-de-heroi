0de0ae00a1fa684b87f6434e60c0071f22318d9d
Fluisaoo <fluis7182@gmail.com>
2025-08-26 00:07:04 -0300

    correçao de codigo

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let playername = "soldadox"
-let experiencia = number(prompt("digite o valor da sua experiencia"))
+let experiencia = Number(prompt("digite o valor da sua experiencia"))
 if (experiencia <= 1000) {
-console.log("seu nivel e ferro")
+console.log("o jogador " + playername + " seu nivel e ferro")
 }else if ( experiencia <= 2000) {
-    console.log("seu nivel e bronze")
+    console.log("o jogador " + playername + " seu nivel e bronze")
 }else if ( experiencia <= 5000) {
-    console.log("seu nivel e prata ")
+    console.log("o jogador " + playername + " seu nivel e prata ")
 }else if ( experiencia <= 7000) {
-    console.log("seu nivel e ouro")
+    console.log("o jogador " + playername + " seu nivel e ouro")
 }else if ( experiencia <= 8000) {
-    console.log("seu nivel e platina ")
+    console.log("o jogador " + playername + " seu nivel e platina ")
 }else if ( experiencia <= 9000) {
-    console.log("seu nivel e ascendente ")
+    console.log("o jogador " + playername + " seu nivel e ascendente ")
 }else if ( experiencia <= 10000) {
-    console.log("seu nivel e imortal")
+    console.log("o jogador " + playername + " seu nivel e imortal")
 }else {
-    console.log("seu nivel e radiante")
+    console.log("o jogador " + playername + " seu nivel e radiante")
 }
