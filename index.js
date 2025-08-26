6f6517e15df0c02e8da595b121f14b06c8ec9289
Fluisaoo <fluis7182@gmail.com>
2025-08-25 23:45:15 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+let playername = "soldadox"
+let experiencia = number(prompt("digite o valor da sua experiencia"))
+if (experiencia <= 1000) {
+console.log("seu nivel e ferro")
+}else if ( experiencia <= 2000) {
+    console.log("seu nivel e bronze")
+}else if ( experiencia <= 5000) {
+    console.log("seu nivel e prata ")
+}else if ( experiencia <= 7000) {
+    console.log("seu nivel e ouro")
+}else if ( experiencia <= 8000) {
+    console.log("seu nivel e platina ")
+}else if ( experiencia <= 9000) {
+    console.log("seu nivel e ascendente ")
+}else if ( experiencia <= 10000) {
+    console.log("seu nivel e imortal")
+}else {
+    console.log("seu nivel e radiante")
+}
